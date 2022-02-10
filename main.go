ed930ed651563d02e5680d308a0c4a20c89fa59a
Fergal <fgribben+fergloragain@gmail.com>
2022-02-10 02:15:27 +0000

    Basic block chain implementation

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
 
+type BlockChain struct {
+	blocks []*Block
+}
+
+func (c *BlockChain) AddBlock(data string) {
+	prevBlock := c.blocks[len(c.blocks)-1]
+	b := CreateBlock(data, prevBlock.Hash)
+	c.blocks = append(c.blocks, b)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{blocks: []*Block{Genesis()}}
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	b := &Block{
+		Data:     []byte(data),
+		PrevHash: prevHash,
+	}
+	b.DeriveHash()
+	return b
+}
 func main() {
-	fmt.Println("Hello, world")
+	chain := InitBlockChain()
+	chain.AddBlock("First book")
+	chain.AddBlock("Second book")
+	chain.AddBlock("Third book")
+
+	for _, b := range chain.blocks {
+		fmt.Printf("Previous Hash: %x\n", b.PrevHash)
+		fmt.Printf("Data in Block: %s\n", b.Data)
+		fmt.Printf("Hash: %x\n", b.Hash)
+	}
 }
